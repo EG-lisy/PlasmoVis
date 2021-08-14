f96ef115e45f77ad1657657bda004e074c03721f
EG-lisy <elisabetta.galatola@cranfield.ac.uk>
2021-08-14 13:04:11 +0100

    PlasmoVis

--- project/app.js
+++ project/app.js
@@ -1,7 +1,7 @@
-// DEV: run server using: npm start from project directory("start": "nodemon app.js" set on json file)
-
-/* HTML files have been converted into EJS files (Embedded JavaScript templating) - easier to render
- EJS is a simple templating language that lets you generate HTML markup with plain JavaScript */
+// DEV note: run server using: npm start from project directory("start": "nodemon app.js" set on json file)
+// Note 1: MongoDB code has been commented out
+// Note 2:
+// all HTML files have been converted into EJS files (Embedded JavaScript templating)
 
 'use strict'  // code executes in strict mode: udeclared variables are not allowed
 
@@ -11,96 +11,97 @@ const router = require('./router'); // json outputs
 const app = express();
 const port = 3000;
 
-//=============================== provide a connect/express middlewhere
+// provide a connect/express middlewhere
 const cors = require('cors');
 app.use(cors());
-//===========================================================================
 
-//START (not used)
-//import mongoose //NOT USED
-const mongoose = require('mongoose'); //mongoose obj to connect to the db
-//import model
-const Metadata = require('./models/data');
-const { prototype } = require('events');
+// // ===========================================================================
+// MONGODB (not used)
+// //import mongoose
+// const mongoose = require('mongoose'); //mongoose obj to connect to the db
+// //import model
+// const Metadata = require('./models/data');
+// const { prototype } = require('events');
 
-//DATABASE (MongoDB) //NOT USED
-const mongodb = "mongodb+srv://lisy:test1234@cluster0.lzzno.mongodb.net/plasmo-vis?retryWrites=true&w=majority"; //connect to DB - plasmo-vis (collection=user)
-//asyncronic task: will take some time (check console)
-mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true }) //connect, second arg to stop deprecation warning
-    .then((result) => console.log('Succesfully connected to DB!\nVisit Home Page: http://localhost:' + port +
-        '\n\n--------------------------------METADATA--------------------------------\n' +
-        'Locations: http://localhost:3000/api/locations'))
-    //catch error
-    .catch((err) => console.log(err));
-// we want server to listen to requests only after connection has been made, otherwise user will not be able to see data -> listen until the latter has been completed
-//END===========================================================================
+// // NOTE: username and MongoDB password have bee deliberately changed!
+// const mongodb = "mongodb+srv://USERNAME:PASSWORD@cluster0.lzzno.mongodb.net/plasmo-vis?retryWrites=true&w=majority"; //connect to DB - plasmo-vis (collection=user)
 
-// check router js - metadata queries
-app.use('/api', router);
+// //asyncronic task: will take some time (check console)
+// mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true }) //connect, second arg to stop deprecation warning
+//     .then((result) => console.log('Succesfully connected to DB!\nVisit Home Page: http://localhost:' + port +
+//         '\n\n--------------------------------METADATA--------------------------------\n' +
+//         'JQUERY eg: http://localhost:3000/api/locations'))
+//     //catch error
+//     .catch((err) => console.log(err));
 
-// Access folders / Static files
+// // we want server to listen to requests only after connection has been made, otherwise user will not be able to see data -> listen until the latter has been completed
+// // ===========================================================================
+
+app.use('/api', router); // check router js - metadata queries (PK)
+
+// // ===========================================================================
+// Access folders - serve static files
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
 
-//static data (e.g. localhost:3000/pk_data/filteredERR274221_bqsr.vcf.gz)
+//static data (e.g. http://localhost:3000/pk_data/filteredERR274221_bqsr.vcf.gz)
 app.use('/pk_data', express.static(__dirname + 'public/pk_data'));
 
-// app.get('/upload', function(req, res) {
-//     res.sendfile('views/upload.txt');
-//   });
+//static data (e.g. http://localhost:3000/pm_data/Pmalariae_fullgenome.fasta
+app.use('/pm_data', express.static(__dirname + 'public/pm_data'));
 
 //json directory
 app.use(express.static('views'))
 app.use('/json', express.static(__dirname + 'views/json'));
 
-
 // Set Views
 app.set('views', './views');
 app.set('view engine', 'ejs')
+// // ===========================================================================
 
+// // ===========================================================================
+// //START (NOT USED)
+// //mongoose and sandbox routes
+// app.get('/add-metadata', (req, res) => {
+//     //import model from models/data.js (see above)
+//     const metadata = new Metadata({ //using model to create a new instance of Metadata doc
+//         sample: 'sample2',
+//         location: 'location of sample collection',
+//         group_pca: 'grouped id'
+//     });
 
-//START (NOT USED)===========================================================================
-//mongoose and sandbox routes
-app.get('/add-metadata', (req, res) => {
-    //import model from models/data.js (see above)
-    const metadata = new Metadata({ //using model to create a new instance of Metadata doc
-        sample: 'sample2',
-        location: 'location of sample collection',
-        group_pca: 'grouped id'
-    });
+//     metadata.save() //to save metadata on DB using metadata model
+//         .then((result) => {
+//             res.send(result)
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         })
+// });
+// //retrive data
+// app.get('/all-metadata', (req, res) => {
+//     Metadata.find()
+//         .then((result) => {
+//             res.send(result)
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         })
+// })
 
-    metadata.save() //to save metadata on DB using metadata model
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
-//retrive data
-app.get('/all-metadata', (req, res) => {
-    Metadata.find()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
-
-//retrive data - single
-app.get('/single-metadata', (req, res) => {
-    Metadata.findById('60ca21ed450c513761a0e4b2') //unique 
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
-//END===========================================================================
+// //retrive data - single
+// app.get('/single-metadata', (req, res) => {
+//     Metadata.findById('60ca21ed450c513761a0e4b2') //unique 
+//         .then((result) => {
+//             res.send(result)
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         })
+// })
+// // ===========================================================================
 
 app.use(express.urlencoded({ extended: false })) // take forms and build/access inside post
 //Render page - main: login
@@ -108,12 +109,13 @@ app.get('', (req, res) => {
     res.render('index')
 });
 
-//===============================================
-//Render page - main:register //NOT USED
+// // ===========================================================================
+//                              NOT USED - FUTURE WORK (WITH DB)
+//Render page - main:register 
 app.get('/register', (req, res) => {
     res.render('register')
 });
-//Render page - main:login //NOT USED
+//Render page - main:login 
 app.get('/login', (req, res) => {
     res.render('login')
 });
@@ -123,70 +125,116 @@ app.post('/login', (req, res) => {
 app.post('/register', (req, res) => {
     req.body.name //requests from /register name 
 });
-//================================================
+// // ===========================================================================
 
-//Render page - index
+// index
 app.get('/index', (req, res) => {
     res.render('index')
 });
 
-app.get('/data', (req, res) => {
-    res.render('tablemetadata')
+// data PK
+app.get('/data_pk', (req, res) => {
+    res.render('data_pk')
 });
 
-//Render page - help
+// data PK
+app.get('/data_pm', (req, res) => {
+    res.render('data_pm')
+});
+
+// HELP --- NOT USED
 app.get('/help', (req, res) => {
     res.render('help')
 });
-//Render page - browser
-app.get('/browser', (req, res) => {
-    res.render('browser')
-});
 
-//IGV
+// // ===========================================================================
+// IGV routes
+// Note: first 3. display (possible) erroneous tracks (Pk and Pm)
+// Please refer TO 4. ONWARDS
+
+//1. PK (all samples)
 app.get('/igv', (req, res) => {
     res.render('igv')
 });
 
-//IGV
-app.get('/tabletest', (req, res) => {
-    res.render('tabletest')
+//2. PK (intersected)
+app.get('/igv_Pk', (req, res) => {
+    res.render('igv_groups_Pk')
 });
 
-// //Render page - browser
-// app.get('/plasmodb', (req, res) => {
-//     res.render('plasmodb')
-// });
+//3. PM (intersected) - FILES RE-INTERSECTED NOT USING ISEC (see igv_pm80 igv_pm60 igv_pm20)
+app.get('/igv_Pm', (req, res) => {
+    res.render('igv_pm')
+});
 
-//render charts
+//4. PM  80% threshold
+app.get('/igv_Pm80', (req, res) => {
+    res.render('igv_pm80')
+});
+//5. PM  60% threshold
+app.get('/igv_Pm60', (req, res) => {
+    res.render('igv_pm60')
+});
+//6. PM  20% threshold
+app.get('/igv_Pm20', (req, res) => {
+    res.render('igv_pm20')
+});
 
+app.get('/map', (req, res) => {
+    res.render('partials/map_PM.ejs')
+});
+// // ===========================================================================
+
+// render charts - PK
 app.get('/groupschart', (req, res) => {
     res.render('partials/chart_groups')
 });
+app.get('/vennchart', (req, res) => {
+    res.render('partials/vennchart')
+});
+
 app.get('/locationschart', (req, res) => {
     res.render('partials/chart_locations')
 });
 
-//Render page - about
+// render charts - PM
+app.get('/chart_PM_groups', (req, res) => {
+    res.render('partials/chart_PM_groups')
+});
+
+app.get('/vennchartpm80', (req, res) => {
+    res.render('partials/vennchartpm80')
+});
+
+app.get('/vennchartpm60', (req, res) => {
+    res.render('partials/vennchartpm60')
+});
+
+app.get('/vennchartpm20', (req, res) => {
+    res.render('partials/vennchartpm20')
+});
+
+// render about page
 app.get('/about', (req, res) => {
     res.render('about')
 });
-//Render page - tabletest
+// render tabletest page
 app.get('/tabletest', (req, res) => {
     res.render('tabletest')
 });
 
-//table_metadata
+// render table_metadata page
 app.get('/tablemetadata', (req, res) => {
     res.render('tablemetadata')
 });
 
-// Listen on port 3000
+// SERVER 
 app.listen(port, function (error) {
     if (error) {
         console.log('Something went wrong', error)
     } else {
         console.log('Server is listening on port: ' +
-            port + '\n\nPlease wait for DB connection...')
+            port + '\n-------------------------------- PlasmoVis --------------------------------' + 
+            '\nWelcome to PlasmoVis!\n1. Open your browser of choice\n2. Visit: http://localhost:' + port)
     }
 });
--- project/router.js
+++ project/router.js
@@ -1,37 +1,29 @@
+// ===========================================================================
+// METADATA ROUTES 
+// P. knowlesi & P. malariae SQLite queries
+// ===========================================================================
+
 const express = require('express'); // Microsoft SQL Server Express (relational database management system) 
 const sqlite3 = require('sqlite3').verbose(); // npm install sqlite3@5.0.0 --save
-
 const metadata_router = express.Router(); // set router 
-const db = new sqlite3.Database('test.sqlite');  // database path
+
+const pk_db = new sqlite3.Database('Pknowlesi_metadata.sqlite');  // database path (for PK)
+const pm_db = new sqlite3.Database('Pmalariae_metadata.sqlite');  // database path (for PK)
+
 
 metadata_router.use(function (req, res, next) {
     console.log('Received request');
     next();
 });
 
-metadata_router.get('/locations', function (req, res) {
-    const query = 'SELECT location, COUNT(location) AS tot_samples FROM metadata GROUP BY location;';
-    db.all(query, [], function (err, rows) {
-        if (err) {
-            throw err;
-        }
-        res.json(rows);
-    });
-});
+// ===========================================================================
+//                      P. knowlesi queries
+// ===========================================================================
 
-metadata_router.get('/groups', function (req, res) {
-    const query = 'SELECT group_pca, COUNT(group_pca) AS groups FROM metadata GROUP BY group_pca;';
-    db.all(query, [], function (err, rows) {
-        if (err) {
-            throw err;
-        }
-        res.json(rows);
-    });
-});
-
+// all metadata info
 metadata_router.get('/metadata', function (req, res) {
     const query = 'SELECT * FROM metadata';
-    db.all(query, [], function (err, rows) {
+    pk_db.all(query, [], function (err, rows) {
         if (err) {
             throw err;
         }
@@ -39,6 +31,108 @@ metadata_router.get('/metadata', function (req, res) {
     });
 });
 
-//maybe another query with all the samples and locations?
+// group by location
+metadata_router.get('/locations', function (req, res) {
+    const query = 'SELECT location, COUNT(location) AS tot_samples FROM metadata GROUP BY location;';
+    pk_db.all(query, [], function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+// group by pca_group
+metadata_router.get('/groups', function (req, res) {
+    const query = 'SELECT group_pca, COUNT(group_pca) AS groups FROM metadata GROUP BY group_pca;';
+    pk_db.all(query, [], function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+
+// ===========================================================================
+//                      P. malariae queries
+// ===========================================================================
+
+// all metadata info
+metadata_router.get('/metadata_pm', function (req, res) {
+    const query = 'SELECT * FROM metadata';
+    pm_db.all(query, [], function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+// group by country
+metadata_router.get('/country_pm', function (req, res) {
+    const query = 'SELECT country, COUNT(country) AS counts FROM metadata GROUP BY country;'
+    pm_db.all(query, [], function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+// group by continent
+metadata_router.get('/continent_pm', function (req, res) {
+    const query =  'SELECT continent, COUNT(continent) AS counts FROM metadata GROUP BY continent;';
+    pm_db.all(query, [], function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+// Africa - country counts
+metadata_router.get('/africa_pm', function (req, res) {
+    const query =   'SELECT country, COUNT(country) AS counts FROM metadata WHERE continent="Africa" GROUP BY country;';
+    pm_db.all(query, [], function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+// Asia - country counts
+metadata_router.get('/asia_pm', function (req, res) {
+    const query =   'SELECT country, COUNT(country) AS counts FROM metadata WHERE continent="Asia" GROUP BY country;';
+    pm_db.all(query, [], function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+// Americas - country counts
+metadata_router.get('/americas_pm', function (req, res) {
+    const query =   'SELECT country, COUNT(country) AS counts FROM metadata WHERE continent="Americas" GROUP BY country;';
+    pm_db.all(query, [], function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+// Australasia - country counts
+metadata_router.get('/australasia_pm', function (req, res) {
+    const query =   'SELECT country, COUNT(country) AS counts FROM metadata WHERE continent="Australasia" GROUP BY country;';
+    pm_db.all(query, [], function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
 
 module.exports = metadata_router
